b01b05e6f90131eaa7bcead2398756af4243e9bb
charlie-captain <chavezayne@gmail.com>
2021-01-12 21:08:30 +0800

    auto commit : /Users/charlie/PycharmProjects/auto_push/未命名文件夹

---
+++ auto_push.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+BRANCH="main"
+
+start_moniting()
+{
+    echo "start morniting folder......"
+    fswatch -0 -l 1 ./ -e ".git" | while read -d "" event; do sync_files $event ; done
+}
+
+sync_files()
+{
+    echo "start sync_files"
+   	git add . | git commit -m "auto commit : ${1}" | git fetch origin ${BRANCH}
+}
+
+doOnMac(){
+	echo "mac"
+	if hash fswatch >/dev/null 2>&1
+	then 
+		echo "command exist"
+	else
+		brew install fswatch	
+	fi
+	start_moniting
+}
+
+
+if [ "$(uname)" == "Darwin" ]
+then
+	# Mac OS X 操作系统
+	doOnMac
+elif [ "$(expr substr $(uname -s) 1 5)"=="Linux" ]
+then
+    # GNU/Linux操作系统
+	echo "linux"
+elif [ "$(expr substr $(uname -s) 1 10)"=="MINGW32_NT" ]
+then
+    # Windows NT操作系统
+	echo "windows"
+fi
